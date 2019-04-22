66ac2cced6f5f03dcf914d0e2d2c192d9f42c335
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-22 17:00:39 +0900

    4월 22일

---
+++ src/color/main.java
@@ -0,0 +1,32 @@
+package color;
+
+public class main {
+	public static void main(String[] a) {
+		System.out.println("변수 시작!");
+		
+		String 문자열 ="Hello World"; //변수 선언과 대입이 동시에 이루어진 것.
+		
+		System.out.println(문자열); //변수 사용
+		
+        문자열 = "문자열 변경"; //값 변경
+        
+        System.out.println(문자열);// 변수 사용
+        
+        문자열 = "1234"; //연산
+        
+        System.out.println(문자열); //변수 사용
+        
+        
+        
+        int 정수 = 1;
+        System.out.println(정수);
+        
+        
+       String 결과값 = 문자열 + (정수 + 정수 + 정수) + 문자열;
+       System.out.println(결과값);
+        
+        
+	}
+	
+
+}
