7ff506da1102bb59ae450e209db6d5bdbe44e377
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-23 16:07:35 +0900

    메소드 계산기

---
+++ src/color0423/Cal.java
@@ -0,0 +1,18 @@
+package color0423;
+
+public class Cal {
+
+	public static void main(String[] args) {
+		
+		
+System.out.println(덧셈(5, 3));
+	}
+	
+	
+  static int 덧셈(int a, int b) {
+	return a + b;
+	
+}
+		
+		
+}
