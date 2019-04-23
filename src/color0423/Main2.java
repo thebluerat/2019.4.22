172d83670e1dc631afac7186ffb167231073ab57
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-23 15:41:22 +0900

    특수 문자 연습

---
+++ src/color0423/Main2.java
@@ -0,0 +1,21 @@
+package color0423;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		System.out.println("특수 문자 출력");
+		/*********************
+		 * 특수 문자>> \?
+		 * \n : 한 줄 이동
+		 * \' : ' >문자를 정의할 때 쓴다('')
+		 * \uD64D : 홍
+		 *******************/
+		
+
+		System.out.println("출력 결과 : \u005c\u005c");
+		
+	
+
+	}
+
+}
