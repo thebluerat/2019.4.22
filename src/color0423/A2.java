08d445e759d5ba8b571a89da96669baac3d6cdd9
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-23 18:09:07 +0900

    연습!

--- src/color0423/A2.java
+++ src/color0423/A2.java
@@ -1,12 +1,15 @@
 package color0423;
 
 public class A2 {
+	//2
 	public A2() {
+		//3
 		new B2 ("안녕하쇼!");
 	}
 	
 	public A2 (String msg) {
-		System.out.println("B2 class : " + msg);
+		//8
+		System.out.println("B2 class : " + msg);//9
 	}
 
 }
--- src/color0423/Start2.java
+++ src/color0423/Start2.java
@@ -3,7 +3,7 @@ package color0423;
 public class Start2 {
 
 	public static void main(String[] args) {
-		new A2();
+		new A2();//1
 
 	}
 
