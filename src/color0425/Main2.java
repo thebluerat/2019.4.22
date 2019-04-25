8be8f5293094b2c1ade8bc5f8df98c3702517810
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-25 18:21:46 +0900

    구구단

---
+++ src/color0425/Main2.java
@@ -0,0 +1,34 @@
+package color0425;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		/**********
+		 * 반복문 연습: for문, while문
+		 */
+반복문1 for문 = new 반복문1();
+for문.t1();//0~9
+
+for문.t2(0);//
+for문.t2(5);//0~4
+
+for문.t3(1, 3); //1, 2
+for문.t3(3, 7); //3, 4, 5, 6
+for문.t3(4, 2); //
+
+for문.t4();
+
+for문.t5(); //중첩 for문 시작
+
+for문.t6();
+
+for문.t7();
+
+for문.t8();
+
+for문.t9();
+
+
+	}
+
+}
---
+++ src/color0425/반복문1.java
@@ -0,0 +1,140 @@
+package color0425;
+
+public class 반복문1 {
+/***********************************
+ * for문: for(1,2,3) {실행문}
+ * 1)초기화식: for문에서 사용할 변수 선언 및 초기값을 정의한다
+ * 2)조건식: if문과 동일하게 거짓을 찾기 위한 조건
+ * 3)증감식: for문에서 사용할 변수에 변화를 주기 위한 식
+ ****************************************/
+	
+	public void t1() {
+		for(int i = 0; i < 10; i ++) {
+			System.out.println(i);//0~9 예상
+		}
+	}
+	
+	public void t2(int s) {
+		System.out.println("반복문 시작");
+		for(int i = 0; i < s ; i++) {
+			System.out.println(i);
+		}
+		System.out.println("반복문 종료");
+	}
+	
+	public void t3(int a, int b) {
+		System.out.println("t3 반복문 시작");
+		for(; a < b;a++) {//밖에서 만들었기 때문에(int a, int b) 그냥 ;
+			System.out.println(a);
+		}
+		System.out.println("t3 반복문 종료");
+	}
+	
+	
+	public void t4() {//0 ~ 4 
+		for(int i = 0;  i < 5; i++) {
+			if(i % 2 == 0) {
+			System.out.print("★");
+		}else {
+			System.out.print("☆");
+		}
+			System.out.println("");
+	  }
+	
+     }
+	
+	public void t5() {
+		for(int i = 0; i < 5; i++) {
+			
+			for(int j = 0; j <= i; j++) {
+				if(j % 2 == 0) {
+					System.out.print("★");
+				}else {
+					System.out.print("☆");
+				}
+				
+			}
+			
+			System.out.println("");
+		}
+	}
+	
+	
+	public void t6(){
+for(int i = 0; i < 5; i++) {
+			
+			for(int j = 0; j <= i; j++) {
+				if((j+i) % 2 == 0) {
+					System.out.print("★");
+				}else {
+					System.out.print("☆");
+				}
+				
+			}
+			
+			System.out.println("");
+		}
+	}
+	
+	
+	public void t7() {
+		int t = 0;
+		for(int i = 0; i < 9; i++) {
+			if(i > 4) {
+				t = t - 2;
+		}
+		for(int j = 0; j <= t; j++) {
+			if(j % 2 == 0) {
+				System.out.print("★");
+			}else {
+				System.out.print("☆");
+			}
+		}
+		
+		t++;
+		System.out.println("");
+
+		}
+    }
+	
+	public void t8() {
+		int t = 0;
+		for (int i = 0; i < 9; i++) {
+			if(i>4) {
+				t=t-2;
+			}
+		
+		for(int j=0; j <= t; j++) {
+			if((j+t)% 2 == 0) {
+				System.out.print("★");
+			}else {
+				System.out.print("☆");
+			}
+		}
+		t++;
+		System.out.println("");
+	}
+	}
+	
+	public void t9() {
+		for(int i = 1; i < 10; i++) {
+			System.out.println( i + "단");
+			for(int j = 1; j<10; j++) {
+				System.out.print(i + " * " + j + " = " + i*j);
+				System.out.println("\t");
+				System.out.print(i + " * " + j + " = " + i*j);
+				
+			}
+		}System.out.println();
+	}
+
+}
+		
+	
+		
+	
+	
+	
+	
+	
+
