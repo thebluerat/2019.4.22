56d3e5d65ae37ceb9c25a4114722fede4ea997d4
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-29 16:29:22 +0900

    dd

--- src/color0429/Main.java
+++ src/color0429/Main.java
@@ -5,9 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		//배열 연습
 		배열 표 = new 배열();
-		표.t1();
-		표.t2();
-		표.t3();
+	//	표.t1();
+		//표.t2();
+		//표.t3();
+	//	표.t4();
+		//boolean result = 표.t5(2,1,1,1);
+	//	System.out.println(result);
+		표.t6();
+		
 
 	}
 
--- src/color0429/배열.java
+++ src/color0429/배열.java
@@ -1,5 +1,7 @@
 package color0429;
 
+import java.util.Scanner;
+
 public class 배열 {
 	
 	public void t1() {
@@ -83,15 +85,116 @@ System.out.println();
 			for(int x = 0; x<표[y].length; x++) {//열 실행
 				System.out.print(표[y][x] + "\t");
 				
-	}
+	         }
 System.out.println();
+	     }
+	
+    }
+	
+	public void t4() {
+		boolean[][] 표 = new boolean[10][10];
+		for(int y = 0; y < 표.length; y++) {
+			for(int x = 0; x < 표[y].length; x++) {
+				if(x==0 || y==0 || x==9 || y==9 ) {
+					
+				}
+				
+			}
+		}
+		
+		for(int y = 0; y<표.length; y++) {//행 실행
+			for(int x = 0; x<표[y].length; x++) {//열 실행
+				System.out.print(표[y][x] + "\t");
+				
+	         }
+System.out.println();
+				}
+	
+			}
+	
+	public boolean  t5(int aX,int aY, int bX,int bY) {
+		boolean result = false;
+		
+		int[][] 맵 = {
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,1,1,1,1,1,1,1,1,0},
+				{0,1,0,0,1,0,1,0,1,0},
+				{0,1,0,0,1,0,1,0,1,0},
+				{0,1,1,0,0,0,1,0,1,0},
+				{0,1,1,0,0,0,1,1,1,0},
+				{0,1,0,0,0,0,1,0,1,0},
+				{0,1,0,0,1,0,1,0,1,0},
+				{0,1,1,1,1,1,1,0,1,0},
+				{0,0,0,0,0,0,0,0,0,0},
+		};
+		
+		for(int y = 0; y < 맵.length; y++) {
+			for(int x = 0; x < 맵[y].length; x++) {
+//				System.out.print(맵[y][x]);
+				
+				if(맵[aY][aX] == 0) {
+					aY = bY;
+					aX = bX; 
+					result = true;
+				}
+				
+				
+				if( y==aY && x==aX) {
+					System.out.print("㉿");
+				}else if(맵[y][x] == 1) {
+					System.out.print("□");
+				}
+				else {
+					System.out.print("■");
+				}
+			}
+			System.out.println();
+		}
+		
+		return result;
+	}
+	
+	public void t6() {
+		Scanner scan = new Scanner(System.in);
+		int aX = 1;
+		int aY = 1;
+		int bX = 1;
+		int bY = 1;
+		t5(aX, aY, bX, bY); //시작을 위하여 필요한 호출 부분
+		while(true) {
+			String input = scan.next();
+			switch(input) {
+			case "w":
+				aY--;
+				break;
+			case "s":
+				aY++;
+				break;
+			case "a":
+				aX--;
+				break;
+			case "d":
+				aX++;
+				break;
+			default:
+				break;
+			if(y==8 && x==8) break;
+			
+			
+			
+			}
+			System.out.println(aY + "," + aX + "," + bY + "," + bX);
+			
+			if(t5(aX, aY, bX, bY)){//이동을 위하여 필요한 호출 부분
+				aX = bX;
+				aY = bY;
+			}else {
+				bX = aX;
+				bY = aY;
+			}
+		}
 	}
 	
 	
 	
-	
-	
-	
-	
-}
 }
